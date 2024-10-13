e7e2e922d3b803ee22437d6500d65de62750c946
shwncho <shwncho4@gmail.com>
2024-10-13 16:40:55 +0900

    mvc 예제 프로젝트 생성

---
+++ spring-mvc/src/main/kotlin/com/example/mvc/controller/HealthController.kt
@@ -0,0 +1,11 @@
+package com.example.mvc.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health")
+    fun health() = "OK"
+}
