228917574edb0789f25571665ab1790d8c9bf3d7
shwncho <shwncho4@gmail.com>
2024-10-24 18:48:37 +0900

    라이브러리 추가

--- coroutine/build.gradle.kts
+++ coroutine/build.gradle.kts
@@ -10,7 +10,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("io.projectreactor:reactor-core:3.5.7")
+
+    implementation("ch.qos.logback:logback-classic:1.4.8")
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 tasks.test {
