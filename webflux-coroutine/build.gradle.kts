6317a8ef7da5f422e23b3e7fee7dfcb9b6e03d12
shwncho <shwncho4@gmail.com>
2024-10-28 20:05:50 +0900

    프로젝트들에 필요한 docker 생성

--- webflux-coroutine/build.gradle.kts
+++ webflux-coroutine/build.gradle.kts
@@ -19,14 +19,15 @@ repositories {
 }
 
 dependencies {
+	implementation("io.github.microutils:kotlin-logging:3.0.5")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("io.r2dbc:r2dbc-h2")
+	testRuntimeOnly("com.h2database:h2")
+	testRuntimeOnly("io.r2dbc:r2dbc-h2")
 	runtimeOnly("org.mariadb:r2dbc-mariadb:1.1.3")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
