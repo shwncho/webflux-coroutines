b6f85a5dcff9b4a368ff1c67015ca02871b48884
shwncho <shwncho4@gmail.com>
2024-12-08 15:37:39 +0900

    local lock을 통해 spin lock 부담 줄이기

--- webflux-coroutine-advanced/src/main/kotlin/com/example/webfluxcoroutine/config/Locker.kt
+++ webflux-coroutine-advanced/src/main/kotlin/com/example/webfluxcoroutine/config/Locker.kt
@@ -6,6 +6,7 @@ import mu.KotlinLogging
 import org.springframework.cache.interceptor.SimpleKey
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeoutException
 import kotlin.time.Duration.Companion.nanoseconds
 import kotlin.time.Duration.Companion.seconds
@@ -16,6 +17,7 @@ class Locker(
     private val template: ReactiveRedisTemplate<Any, Any>
 ) {
     private val logger = KotlinLogging.logger {}
+    private val localLock = ConcurrentHashMap<SimpleKey, Boolean>()
     private val ops = template.opsForValue()
 
     suspend fun <T> lock(key: SimpleKey, work: suspend () -> T): T {
@@ -29,13 +31,17 @@ class Locker(
 
     private suspend fun tryLock(key: SimpleKey): Boolean {
         val start = System.nanoTime()
-        while(!ops.setIfAbsent(key, true, 10.seconds.toJavaDuration()).awaitSingle()) {
+        while(
+            !localLock.contains(key) &&
+            !ops.setIfAbsent(key, true, 10.seconds.toJavaDuration()).awaitSingle()
+        ) {
             delay(100)
             val elapsed = (System.nanoTime() - start).nanoseconds
             if(elapsed>=10.seconds) {
                 return false
             }
         }
+        localLock[key] = true
         return true
     }
 
@@ -44,6 +50,8 @@ class Locker(
             ops.delete(key).awaitSingle()
         } catch(e: Exception) {
             logger.warn(e.message, e)
+        } finally {
+            localLock.remove(key)
         }
     }
 }
