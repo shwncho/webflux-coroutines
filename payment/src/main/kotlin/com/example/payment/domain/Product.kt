b08dbd0a2f1b607452c4602385942373a5f83dff
shwncho <shwncho4@gmail.com>
2024-12-13 20:25:29 +0900

    order, productInOrder 도메인 생성

--- payment/src/main/kotlin/com/example/payment/domain/Product.kt
+++ payment/src/main/kotlin/com/example/payment/domain/Product.kt
@@ -2,11 +2,14 @@ package com.example.payment.domain
 
 import au.com.console.kassava.kotlinEquals
 import au.com.console.kassava.kotlinHashCode
+import au.com.console.kassava.kotlinToString
+import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table(name = "TB_PROD")
 class Product(
-    var id: Long = 0,
+    @Id
+    val id: Long = 0,
     var name: String = "",
     var price: Long = 0,
 ): BaseEntity() {
@@ -21,4 +24,14 @@ class Product(
             Product::id
         ))
     }
+
+    override fun toString(): String {
+        return kotlinToString(arrayOf(
+            Product::id,
+            Product::name,
+            Product::price,
+        ), superToString = { super.toString() })
+    }
+
+
 }
