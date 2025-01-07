06e3c0b9ebb64a0145f51efca3d507e14e1785c5
shwncho <shwncho4@gmail.com>
2025-01-07 16:24:22 +0900

    es 프로젝트 config 설정 및 yml 설정

--- elasticsearch/build.gradle.kts
+++ elasticsearch/build.gradle.kts
@@ -19,6 +19,13 @@ repositories {
 }
 
 dependencies {
+
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+    implementation("io.micrometer:context-propagation:1.0.5")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.7.3")
+
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
---
+++ elasticsearch/src/main/kotlin/com/example/elasticsearch/config/validator/DateValidator.kt
@@ -0,0 +1,31 @@
+package com.example.elasticsearch.config.validator
+
+import com.example.elasticsearch.config.extension.toLocalDate
+import com.example.elasticsearch.config.extension.toString
+
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [DateValidator::class])
+annotation class DateString(
+    val message: String = "not a valid date",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
+
+class DateValidator: ConstraintValidator<DateString, String> {
+    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
+        // 20230901 -> yyyyMMdd
+        val text = value?.filter { it.isDigit() } ?: return true
+        return runCatching {
+            text.toLocalDate().let {
+                if(text != it.toString("yyyyMMdd")) null else true
+            }
+        }.getOrNull() != null
+    }
+}
