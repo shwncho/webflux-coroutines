e9677395de0912a8b29c4825ecee3ca45f4d4d9a
shwncho <shwncho4@gmail.com>
2025-01-21 22:58:35 +0900

    java nio server 프로젝트 생성

---
+++ practices/nio-server/src/main/java/com/example/nioserver/NioServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nioserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NioServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NioServerApplication.class, args);
+    }
+
+}
---
+++ practices/nio-server/src/test/java/com/example/nioserver/NioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
