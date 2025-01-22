04fd9837db3db8a9b3d27841139d334272d50526
shwncho <shwncho4@gmail.com>
2025-01-22 22:05:00 +0900

    TCP 기반 간단한 Java I/O 서버

---
+++ practices/nio-server/src/main/java/com/example/nioserver/JavaIOClient.java
@@ -0,0 +1,29 @@
+package com.example.nioserver;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+@Slf4j
+public class JavaIOClient {
+    @SneakyThrows
+    public static void main(String[] args) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress("127.0.0.1", 8080));
+
+            OutputStream out = socket.getOutputStream();
+            String requestBody = "This is Client";
+            out.write(requestBody.getBytes());
+            out.flush();
+
+            InputStream in = socket.getInputStream();
+            byte[] responseBytes = new byte[1024];
+            in.read(responseBytes);
+            log.info("result: {}", new String(responseBytes).trim());
+        }
+    }
+}
