9f35659d3e6dd0ccbe98f44259006ebc0c300d0e
shwncho <shwncho4@gmail.com>
2025-02-01 18:43:07 +0900

    proactor pattern 프로젝트 생성

---
+++ practices/proactor-pattern/src/test/java/com/example/proactorpattern/ProactorPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proactorpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProactorPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
