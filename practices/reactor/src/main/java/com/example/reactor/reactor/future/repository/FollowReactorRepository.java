ab65beaa82e61f13875eefd240bc3c2af42e6562
shwncho <shwncho4@gmail.com>
2025-02-11 21:21:37 +0900

    completable futrue 코드 기반 reactor 디렉토리 생성

--- practices/reactor/src/main/java/com/example/reactor/reactor/future/repository/FollowReactorRepository.java
+++ practices/reactor/src/main/java/com/example/reactor/reactor/future/repository/FollowReactorRepository.java
@@ -1,4 +1,4 @@
-package com.example.reactor.future.repository;
+package com.example.reactor.reactor.future.repository;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -7,16 +7,16 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
-public class FollowFutureRepository {
+public class FollowReactorRepository {
     private Map<String, Long> userFollowCountMap;
 
-    public FollowFutureRepository() {
+    public FollowReactorRepository() {
         userFollowCountMap = Map.of("1234", 1000L);
     }
 
     @SneakyThrows
     public CompletableFuture<Long> countByUserId(String userId) {
-        return CompletableFuture.supplyAsync( () -> {
+        return CompletableFuture.supplyAsync(() -> {
             log.info("FollowRepository.countByUserId: {}", userId);
             try {
                 Thread.sleep(1000);
---
+++ practices/reactor/src/main/java/com/example/reactor/reactor/future/repository/UserReactorRepository.java
@@ -0,0 +1,34 @@
+package com.example.reactor.reactor.future.repository;
+
+import com.example.reactor.common.repository.UserEntity;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class UserReactorRepository {
+    private final Map<String, UserEntity> userMap;
+
+    public UserReactorRepository() {
+        var user = new UserEntity("1234", "taewoo", 32, "image#1000");
+
+        userMap = Map.of("1234", user);
+    }
+
+    @SneakyThrows
+    public CompletableFuture<Optional<UserEntity>> findById(String userId) {
+        return CompletableFuture.supplyAsync(() -> {
+            log.info("UserRepository.findById: {}", userId);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            var user = userMap.get(userId);
+            return Optional.ofNullable(user);
+        });
+    }
+}
