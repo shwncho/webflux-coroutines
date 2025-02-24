f580d01b5f5e5acb7896bff9d79f30b8e1269a79
shwncho <shwncho4@gmail.com>
2025-02-24 19:11:11 +0900

    annotation을 통한 간단한 webflux 요청 처리

---
+++ examples/spring-webflux/src/main/java/com/example/springwebflux/annotated/AnnotatedApplication.java
@@ -0,0 +1,11 @@
+package com.example.springwebflux.annotated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotatedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotatedApplication.class, args);
+    }
+}
