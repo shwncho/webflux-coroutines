c3507daf99c63154db986ce20072140bc94f41ca
shwncho <shwncho4@gmail.com>
2025-02-27 19:01:01 +0900

    webflux image 프로젝트 생성

---
+++ practices/webflux-image/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
+    implementation("org.springframework.security:spring-security-core")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // reactor tool
+    implementation("io.projectreactor:reactor-tools")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ practices/webflux-image/src/test/java/com/example/webfluximage/WebfluxImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluximage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
