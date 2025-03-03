25be9dad5c527c56c9f07fb6e38e95ca2cf1030e
shwncho <shwncho4@gmail.com>
2025-03-03 16:40:06 +0900

    없는 유저일 경우 Mono error 404 반환

--- practices/webflux/src/main/java/com/example/webflux/controller/UserController.java
+++ practices/webflux/src/main/java/com/example/webflux/controller/UserController.java
@@ -3,10 +3,12 @@ package com.example.webflux.controller;
 import com.example.webflux.controller.dto.UserResponse;
 import com.example.webflux.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
@@ -25,6 +27,6 @@ public class UserController {
                         user.getAge(),
                         user.getFollowCount()
                     )
-                );
+                ).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 }
