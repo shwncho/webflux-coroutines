69abec1082935c588e8f1b0d1fbafe566d28fa68
shwncho <shwncho4@gmail.com>
2025-03-12 19:19:59 +0900

    Reactor sink 로직을 service로 분리

--- practices/sse/src/main/java/com/example/sse/controller/NotificationController.java
+++ practices/sse/src/main/java/com/example/sse/controller/NotificationController.java
@@ -1,23 +1,25 @@
 package com.example.sse.controller;
 
+import com.example.sse.service.NotificationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.Sinks;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/notifications")
 @RestController
 public class NotificationController {
-    private static Sinks.Many<String> sink = Sinks.many().unicast().onBackpressureBuffer();
+    private final NotificationService notificationService;
     private static AtomicInteger lastEventId = new AtomicInteger(1);
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ServerSentEvent<String>> getNotifications() {
-        return sink.asFlux()
+        return  notificationService.getMessageFromSink()
                 .map(message -> {
                     String id = lastEventId.getAndIncrement() + "";
                     return ServerSentEvent
@@ -32,7 +34,7 @@ public class NotificationController {
     @PostMapping
     public Mono<String> addNotification(@RequestBody Event event) {
         String notificationMessage = event.getType() + ": " + event.getMessage();
-        sink.tryEmitNext(notificationMessage);
+        notificationService.tryEmitNext(notificationMessage);
 
         return Mono.just("ok");
     }
