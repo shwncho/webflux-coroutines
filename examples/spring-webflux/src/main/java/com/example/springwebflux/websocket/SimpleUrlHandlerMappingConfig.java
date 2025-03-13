921c182d784f544d875ed0e335de3baba9a7c841
shwncho <shwncho4@gmail.com>
2025-03-13 20:24:11 +0900

    simple url handerl mapping config 등록

---
+++ examples/spring-webflux/src/main/java/com/example/springwebflux/websocket/SimpleUrlHandlerMappingConfig.java
@@ -0,0 +1,22 @@
+package com.example.springwebflux.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+
+import java.util.Map;
+
+@Configuration
+public class SimpleUrlHandlerMappingConfig {
+    @Bean
+    public SimpleUrlHandlerMapping simpleUrlHandlerMapping() {
+        Map<String, Object> urlMap = Map.of(
+
+        );
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(1);
+        mapping.setUrlMap(urlMap);
+
+        return mapping;
+    }
+}
