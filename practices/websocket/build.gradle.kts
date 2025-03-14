48ec403121d9095b5ffc2d1fa788799a9a38adb8
shwncho <shwncho4@gmail.com>
2025-03-14 18:10:11 +0900

    reactor tool, lombok 라이브러리 추가

--- practices/websocket/build.gradle.kts
+++ practices/websocket/build.gradle.kts
@@ -21,9 +21,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // reactor tool
+    implementation("io.projectreactor:reactor-tools")
+
+    // Lombok 추가
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
