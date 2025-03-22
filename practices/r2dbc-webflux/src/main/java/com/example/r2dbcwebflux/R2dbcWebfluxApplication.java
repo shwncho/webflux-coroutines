e5b9843835aee32004ab22de9ce0b3d21595e65c
shwncho <shwncho4@gmail.com>
2025-03-22 21:09:33 +0900

    r2dbc 프로젝트 생성

---
+++ practices/r2dbc-webflux/src/main/java/com/example/r2dbcwebflux/R2dbcWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.r2dbcwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R2dbcWebfluxApplication.class, args);
+    }
+
+}
---
+++ practices/r2dbc-webflux/src/test/java/com/example/r2dbcwebflux/R2dbcWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.r2dbcwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
