5d8983d438366f1594fdb07156be257f1e5fe565
shwncho <shwncho4@gmail.com>
2025-03-30 23:01:51 +0900

    reactive mongo에 reactor tool 라이브러리 추가

--- practices/mongo-chat/build.gradle.kts
+++ practices/mongo-chat/build.gradle.kts
@@ -22,9 +22,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // reactor tool
+    implementation("io.projectreactor:reactor-tools")
 }
 
 tasks.withType<Test> {
