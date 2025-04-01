99ef961bea70ab52dd753b14aabd37971caaf38b
shwncho <shwncho4@gmail.com>
2025-04-01 18:24:37 +0900

    websocket 기반 채팅 로직 작성

---
+++ practices/mongo-chat/src/main/java/com/example/mongochat/config/MappingConfig.java
@@ -0,0 +1,26 @@
+package com.example.mongochat.config;
+
+import com.example.mongochat.handler.ChatWebSocketHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+
+import java.util.Map;
+
+@Configuration
+public class MappingConfig {
+
+    @Bean
+    SimpleUrlHandlerMapping simpleUrlHandlerMapping(ChatWebSocketHandler chatWebSocketHandler) {
+        Map<String, WebSocketHandler> map = Map.of(
+                "/chat", chatWebSocketHandler
+        );
+
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(1);
+        mapping.setUrlMap(map);
+
+        return mapping;
+    }
+}
---
+++ practices/mongo-chat/src/main/java/com/example/mongochat/handler/ChatWebSocketHandler.java
@@ -0,0 +1,42 @@
+package com.example.mongochat.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RequiredArgsConstructor
+@Component
+public class ChatWebSocketHandler implements WebSocketHandler {
+    private final ChatService chatService;
+
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        String iam = (String) session.getAttributes().get("iam");
+
+        Flux<Chat> chatFlux = chatService.register(iam);
+        chatService.sendChat(iam,
+                new Chat(iam + "님 채팅방에 오신 것을 환영합니다","system"));
+
+        session.receive()
+                .doOnNext(webSocketMessage -> {
+                    String payload = webSocketMessage.getPayloadAsText();
+
+                    String[] splits = payload.split(":");
+                    String to = splits[0].trim();
+                    String message = splits[1].trim();
+
+                    boolean result = chatService.sendChat(to, new Chat(message, iam));
+                    if(!result) {
+                        chatService.sendChat(iam, new Chat("존재하지 않는 대화 상대 입니다.", "system"));
+                    }
+                }).subscribe();
+
+        return session.send(chatFlux
+                .map(chat -> session.textMessage(chat.getFrom() + ": " + chat.getMessage()))
+        );
+    }
+}
