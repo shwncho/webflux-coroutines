8846a265c2a755f0c9075dc0831d4d8cd2dfb397
shwncho <shwncho4@gmail.com>
2025-04-02 19:16:50 +0900

    chat document

--- practices/mongo-chat/src/main/java/com/example/mongochat/MongoChatApplication.java
+++ practices/mongo-chat/src/main/java/com/example/mongochat/MongoChatApplication.java
@@ -2,7 +2,9 @@ package com.example.mongochat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
+@EnableReactiveMongoAuditing
 @SpringBootApplication
 public class MongoChatApplication {
 
---
+++ practices/mongo-chat/src/main/java/com/example/mongochat/entity/ChatDocument.java
@@ -0,0 +1,35 @@
+package com.example.mongochat.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document
+@AllArgsConstructor
+public class ChatDocument {
+    @Id private final ObjectId objectId;
+    private final String from;
+    private final String to;
+    private final String message;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public ChatDocument(String from, String to, String message) {
+        this(null,from,to,message,null,null);
+    }
+    public ChatDocument withId(ObjectId id)
+    {
+        return new ChatDocument(id, this.from, this.to, this.message, this.createdAt, this.updatedAt);
+    }
+
+}
